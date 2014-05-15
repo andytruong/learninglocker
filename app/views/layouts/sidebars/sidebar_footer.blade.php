c8e082f45b28f17751dbd94067aa6ca1646f217c
Andy Truong <andy@toila.net>
2014-05-15 11:07:37 +0700

    Fix LRS-58 by Canh Tran.

--- app/views/layouts/sidebars/sidebar_footer.blade.php
+++ app/views/layouts/sidebars/sidebar_footer.blade.php
@@ -3,7 +3,7 @@
   <?php $site = Site::first(); ?>
   <div id="footer">
     Powered by
-    @if( isset($site) && ($site->footer_sitename === '' || $site->footer_url === '' ))
+    @if( empty($site->footer_sitename) || empty($site->footer_url))
     	<a href="http://adurolms.com" target='_blank'>Aduro LRS</a>
     @else
    		<a href="{{ $site->footer_url }}" target='_blank'>{{ $site->footer_sitename }}</a>
