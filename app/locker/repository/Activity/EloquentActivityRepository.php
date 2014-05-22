99d2694ff31a3eca2447ed655a2c831186bcd891
Dave Tosh <davidgtosh@gmail.com>
2014-05-22 11:44:53 -0400

    Fix #151: Always update activity definition.

--- app/locker/repository/Activity/EloquentActivityRepository.php
+++ app/locker/repository/Activity/EloquentActivityRepository.php
@@ -1,47 +1,52 @@
-<?php namespace Locker\Repository\Activity;
+<?php
+
+namespace Locker\Repository\Activity;
 
 use Activity;
 
 class EloquentActivityRepository implements ActivityRepository
 {
-  /**
-  * Activity
-  */
-  protected $activity;
 
-  /**
-   * Construct
-   *
-   * @param Activity $activity
-   */
-  public function __construct(Activity $activity)
-  {
-    $this->activity = $activity;
+    /**
+     * Activity
+     */
+    protected $activity;
 
-  }
-
-  public function saveActivity($activity_id, $activity_def)
-  {
-    $exists = \DB::table('activities')->find( $activity_id );
-
-    //if the object activity exists, return details on record.
-    if ($exists) {
-      return $exists['definition'];
-    } else {
-      //save it
-      \DB::table('activities')->insert(
-        array('_id'        => $activity_id,
-              'definition' => $activity_def)
-      );
-
-      return $activity_def;
+    /**
+     * Construct
+     *
+     * @param Activity $activity
+     */
+    public function __construct(Activity $activity)
+    {
+        $this->activity = $activity;
     }
 
-  }
+    public function saveActivity($activity_id, $activity_def)
+    {
+        $exists = \DB::table('activities')->find($activity_id);
 
-  public function getActivity($activity_id)
-  {
-    return  \DB::table('activities')->where('_id', $activity_id)->first();
-  }
+        // if the object activity exists, return details on record.
+        if ($exists) {
+            //update record
+            \DB::table('activities')
+                ->where('_id', $activity_id)
+                ->update(array('definition' => $activity_def));
+        }
+        else {
+            // save new record
+            \DB::table('activities')->insert(
+                array('_id' => $activity_id,
+                    'definition' => $activity_def)
+            );
+
+            return $activity_def;
+        }
+    }
+
+    public function getActivity($activity_id)
+    {
+        return \DB::table('activities')->where('_id', $activity_id)->first();
+    }
 
 }
