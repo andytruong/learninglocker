9ad6cf26f8a01f5c992cc4356f39ba973bce1f7c
Andy Truong <thehongtt@gmail.com>
2014-05-23 16:45:42 +0700

    For LRS-128: Add todo item.

--- app/controllers/api/BaseController.php
+++ app/controllers/api/BaseController.php
@@ -1,103 +1,109 @@
-<?php namespace Controllers\API;
+<?php
+
+namespace Controllers\API;
 
 use Illuminate\Routing\Controller;
 
 class BaseController extends Controller
 {
-public function CORSOptions()
-{
-  $statusCode = 204;
-  $headers = [
-      'Access-Control-Allow-Origin'      => 'http://mydomain.com',
-      'Access-Control-Allow-Methods'     => 'GET, PUT, POST, DELETE, OPTIONS',
-      'Access-Control-Allow-Headers'     => 'Origin, Content-Type, Accept, Authorization, X-Requested-With, X-Experience-API-Version, X-Experience-API-Consistent-Through, Updated',
-      'Access-Control-Allow-Credentials' => 'true'
-  ];
 
-  return \Response::make(null, $statusCode, $headers);
-}
+    /**
+     * Callback for /data/xAPI/{extra}
+     *
+     * @todo Replace mydomain.com by web application domain.
+     * @todo Need test case
+     */
+    public function CORSOptions()
+    {
+        $statusCode = 204;
+        $headers = [
+            'Access-Control-Allow-Origin' => 'http://mydomain.com',
+            'Access-Control-Allow-Methods' => 'GET, PUT, POST, DELETE, OPTIONS',
+            'Access-Control-Allow-Headers' => 'Origin, Content-Type, Accept, Authorization, X-Requested-With, X-Experience-API-Version, X-Experience-API-Consistent-Through, Updated',
+            'Access-Control-Allow-Credentials' => 'true'
+        ];
 
-public function returnJSON( $data=array(), $additional_params=array(), $extra=array(), $debug=array() )
-{
-    $json = array(
-        'version'   =>  \Config::get('api.using_version'),
-        'route'     =>  \Request::path()
-    );
-
-    $json['url_params'] = \Route::getCurrentRoute()->parameters();
-
-    $params = $this->params;
-    if ( isset($this->params['filter']) ) {
-      $params['filter'] = json_decode( $this->params['filter'] );
+        return \Response::make(null, $statusCode, $headers);
     }
 
-    if ( sizeof($additional_params) > 0 ) {
-      $params = array_merge( $params, $additional_params);
+    public function returnJSON($data = array(), $additional_params = array(), $extra = array(), $debug = array())
+    {
+        $json = array(
+            'version' => \Config::get('api.using_version'),
+            'route' => \Request::path()
+        );
+
+        $json['url_params'] = \Route::getCurrentRoute()->parameters();
+
+        $params = $this->params;
+        if (isset($this->params['filter'])) {
+            $params['filter'] = json_decode($this->params['filter']);
+        }
+
+        if (sizeof($additional_params) > 0) {
+            $params = array_merge($params, $additional_params);
+        }
+
+        $json['params'] = $params;
+
+        if (sizeof($extra) > 0) {
+            $json = array_merge($json, $extra);
+        }
+
+        $json['data'] = $data;
+
+        if (\Config::get('app.debug')) {
+            $json['debug'] = array(
+                'sql' => \DB::getQueryLog()
+            );
+
+            $json['debug'] = array_merge($json['debug'], $debug);
+        }
+
+        return \Response::json($json);
     }
 
-    $json['params'] = $params;
+    /**
+     * GENERIC MODEL HANDLING
+     */
+    protected function findModel($modelType, $id, $with = array())
+    {
+        $model = $modelType::with($with)->find($id);
 
-    if ( sizeof($extra)>0) {
-      $json = array_merge( $json, $extra );
+        if (is_null($model)) {
+            \App::abort(404, 'Model not found');
+        }
+        else {
+            return $model;
+        }
     }
 
-    $json['data'] = $data;
-
-    if ( \Config::get('app.debug') ) {
-      $json['debug'] = array(
-        'sql'   =>  \DB::getQueryLog()
-      );
-
-      $json['debug'] = array_merge($json['debug'], $debug );
+    protected function returnModel($model)
+    {
+        return $this->returnJSON($model->toArray());
     }
 
-    return \Response::json( $json );
-
-  }
-
-  /**
-  * GENERIC MODEL HANDLING
-  **/
-
-  protected function findModel( $modelType, $id, $with=array() )
-  {
-    $model = $modelType::with( $with )->find($id);
-
-    if ( is_null($model)) {
-      \App::abort(404, 'Model not found');
-    } else {
-      return $model;
+    protected function returnSuccessError($success, $message, $code)
+    {
+        return \Response::json(array('success' => $success, 'message' => $message), $code);
     }
 
-  }
+    /**
+     * Get all of the input and files for the request and store them in params.
+     *
+     */
+    public function setParameters()
+    {
+        $this->params = \Request::all();
+    }
 
-  protected function returnModel($model)
-  {
-    return $this->returnJSON( $model->toArray() );
-  }
-
-  protected function returnSuccessError($success, $message, $code)
-  {
-    return \Response::json( array( 'success'  => $success, 'message'  => $message), $code );
-  }
-
-  /**
-   * Get all of the input and files for the request and store them in params.
-   *
-   */
-  public function setParameters()
-  {
-    $this->params = \Request::all();
-  }
-
-  /**
-   * Get the LRS details based on Auth credentials
-   *
-   **/
-  public function getLrs()
-  {
-    //get the lrs
-    $this->lrs = \LrsHelpers::getLrsBySubdomain();
-  }
+    /**
+     * Get the LRS details based on Auth credentials
+     *
+     */
+    public function getLrs()
+    {
+        $this->lrs = \LrsHelpers::getLrsBySubdomain();
+    }
 
 }
