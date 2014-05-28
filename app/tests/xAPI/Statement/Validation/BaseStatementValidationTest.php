7a0ff3aad57801e34347615832e2f43a173e8fc3
Andy Truong <thehongtt@gmail.com>
2014-05-28 09:52:41 +0700

    Fix LRS-75 (continue): Actor › A system consuming Statements MUST NOT assume that Agents in the 'member' property comprise an exact list of Agents in a given anonymous or identified Group.

--- app/tests/xAPI/Statement/Validation/BaseStatementValidationTest.php
+++ app/tests/xAPI/Statement/Validation/BaseStatementValidationTest.php
@@ -8,6 +8,7 @@ use app\locker\statements\xAPIValidation as StatementValidationManager;
 
 abstract class BaseStatementValidationTest extends PHPUnit_Framework_TestCase
 {
+    protected $json_input;
 
     protected function getFixturePath()
     {
@@ -17,9 +18,9 @@ abstract class BaseStatementValidationTest extends PHPUnit_Framework_TestCase
     protected function exec($path)
     {
         $json = file_get_contents($path);
-        $statement = json_decode($json, true);
+        $this->json_input = json_decode($json, true);
         $manager = new StatementValidationManager();
-        return $manager->validate($statement);
+        return $manager->validate($this->json_input);
     }
 
 }
