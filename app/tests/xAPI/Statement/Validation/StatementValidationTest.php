32c8ca534a4d913df186806334daed8b20c0a50a
Andy Truong <thehongtt@gmail.com>
2014-05-28 09:50:48 +0700

    Fix LRS-75: Actor › A system consuming Statements MUST NOT assume that Agents in the 'member' property comprise an exact list of Agents in a given anonymous or identified Group.

--- app/tests/xAPI/Statement/Validation/StatementValidationTest.php
+++ app/tests/xAPI/Statement/Validation/StatementValidationTest.php
@@ -4,19 +4,35 @@ require_once __DIR__ . '/BaseStatementValidationTest.php';
 
 class StatementValidationTest extends BaseStatementValidationTest
 {
+
     /**
      * @dataProvider dataProviderSimple
      */
-    public function testSimple($path) {
+    public function testSimple($path)
+    {
         $results = $this->exec($path);
         $this->assertEquals('passed', $results['status']);
         $this->assertEmpty($results['errors']);
+
+
+        $short_path = substr($path, strpos($path, '/Fixtures/Statements/Valid/') + 27, -5);
+        $extra_method = str_replace(['//', '/', '-'], ' ', trim($short_path, '/'));
+        $extra_method = 'extraChecking' . str_replace(' ', '', ucwords($extra_method));
+        if (method_exists($this, $extra_method)) {
+            $this->{$extra_method}($results);
+        }
     }
 
-    public function dataProviderSimple() {
+    protected function extraCheckingActorGroup($results)
+    {
+        $this->assertEquals($this->json_input['actor']['member'], $results['statement']['actor']['member']);
+    }
+
+    public function dataProviderSimple()
+    {
         $data = [];
 
-        foreach (['', 'Object', 'Verb/Display'] as $k) {
+        foreach (['', 'Actor', 'Object', 'Verb/Display'] as $k) {
             foreach (glob($this->getFixturePath() . "/Valid/{$k}/*.json") as $file) {
                 $data[][] = $file;
             }
@@ -24,4 +40,5 @@ class StatementValidationTest extends BaseStatementValidationTest
 
         return $data;
     }
+
 }
