ec416a7c655cc87b9101a402126595c7cc6ad40f
Andy Truong <thehongtt@gmail.com>
2014-05-28 15:20:15 +0700

    LRS-84: Follow up.

--- app/tests/AuthorityTest.php
+++ app/tests/AuthorityTest.php
@@ -1,39 +1,41 @@
-<?php //
+<?php
 
 /**
  * Test authority in statment
- * 
+ *
  * @see https://github.com/adlnet/xAPI-Spec/blob/master/xAPI.md#authority
  */
-class AuthorityTest extends TestCase {
-    
+class AuthorityTest extends TestCase
+{
+
     public $lrs;
-    
     protected $statement;
-    
-    public function setUp() {
+
+    public function setUp()
+    {
         parent::setUp();
         // Authentication as super user.
         $user = User::firstOrCreate(array('email' => $this->dummyEmail()));
-        Auth::login($user);  
+        Auth::login($user);
         $this->lrs = $this->createLRS();
         $this->statement = App::make('Locker\Repository\Statement\EloquentStatementRepository');
     }
-    
-    public function testAuthority() {
+
+    public function testAuthority()
+    {
         $stmt = $this->defaultStatment();
-        
+
         // Ensure if authority is empty the LRS will be create anonymous authoriry
         $authority = $stmt['authority'];
         unset($stmt['authority']);
         $return = $this->createStatement($stmt, $this->lrs);
         $this->assertEquals($return['success'], true);
-        
+
         $stmt_id = reset($return['ids']);
         $obj_stmt = $this->statement->find($stmt_id);
         $stmt_authority = $obj_stmt->statement['authority'];
         $this->assertTrue(!empty($stmt_authority));
-        
+
         // Ensure authority stored in db is same value with statment send to LRS
         $stmt['authority'] = $authority;
         $return = $this->createStatement($stmt, $this->lrs);
@@ -42,5 +44,5 @@ class AuthorityTest extends TestCase {
         $stmt_authority = $obj_stmt->statement['authority'];
         $this->assertEquals($authority, $stmt_authority);
     }
-    
+
 }
